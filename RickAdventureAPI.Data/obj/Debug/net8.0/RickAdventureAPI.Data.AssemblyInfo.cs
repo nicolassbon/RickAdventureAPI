68a65bd2d7aec58a0f6cf62e94be2ffc4ef0af0a
nicolassbon <nicolasbon019@gmail.com>
2025-11-08 18:22:57 -0300

    fix nombres y estructuras carpetas

--- RickAdventureAPI.Data/obj/Debug/net8.0/RickAdventureAPI.Data.AssemblyInfo.cs
+++ RickAdventureAPI.Data/obj/Debug/net8.0/RickAdventureAPI.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RickAdventureAPI.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3331f0de135a321eeb5ab758a54260159fdfa16")]
 [assembly: System.Reflection.AssemblyProductAttribute("RickAdventureAPI.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RickAdventureAPI.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
